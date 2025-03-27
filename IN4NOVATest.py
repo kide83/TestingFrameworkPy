fc3edd65f3c39d15d1d7776d5fbafd143bc98897
Dejan M. <dejan.mirkovic@in4nova.com>
2025-03-27 10:02:46 +0100

    Deleted Main.py

--- IN4NOVATest.py
+++ IN4NOVATest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 def test_open_in4nova():
     driver = webdriver.Chrome()
     driver.get("https://www.in4nova.com")
-    assert "IN4NOVA" in driver.title
-    driver.quit()
+    # assert "IN4NOVA" in driver.title
+    # driver.quit()
 
-#Testing
+# Testing Again
