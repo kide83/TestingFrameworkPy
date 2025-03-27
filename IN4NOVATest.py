91101197f8b2092c29a87a8e3c5c18e7e010adac
Dejan M. <dejan.mirkovic@in4nova.com>
2025-03-27 09:14:40 +0100

    Initial commit

---
+++ IN4NOVATest.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+def test_open_in4nova():
+    driver = webdriver.Chrome()
+    driver.get("https://www.in4nova.com")
+    assert "IN4NOVA" in driver.title
+    driver.quit()
